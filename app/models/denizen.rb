71f38740f4fa6c93bc94f82bdb61f0f3697095b5
Michael Lawson <lawsmichael@gmail.com>
2017-08-12 18:54:07 -0400

    Added Denizens and elements for Denizens, along with delayed job support

--- app/models/denizen.rb
+++ app/models/denizen.rb
@@ -1,2 +1,10 @@
 class Denizen < ApplicationRecord
+
+  def is_home?
+    if self.location == "home"
+      return true
+    else
+      return false
+    end
+  end
 end
--- app/models/element.rb
+++ app/models/element.rb
@@ -1,3 +1,11 @@
 class Element < ApplicationRecord
   belongs_to :denizen
+
+  def owner_name
+    unless self.denizen_id.blank?
+      return Denizen.find(self.denizen_id).full_name
+    else
+      return nil
+    end
+  end
 end
