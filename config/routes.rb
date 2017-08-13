e7de7d234e0a291322432fe4a0465ce0a67d557d
Michael Lawson <lawsmichael@gmail.com>
2017-08-13 13:38:26 -0400

    Added state changes and history on model level for objects

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'denizens#index'
+  root to: 'landing#index'
 
-  get '/startworker', to: 'denizens#startworker'
+  get '/startworker', to: 'landing#startworker'
   match "/delayed_job" => DelayedJobWeb, :anchor => false, :via => [:get, :post]
   resources :denizens
+  resources :landing
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
