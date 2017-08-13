bd9a57ff43110430c112b127fb3713b4660cd623
jthree2001 <lawsmichael@gmail.com>
2017-08-13 02:18:12 -0400

    Creating landing page

--- lib/workers/homeassistantbase.rb
+++ lib/workers/homeassistantbase.rb
@@ -2,4 +2,5 @@ class HomeAssistantBase
 
   def initialize()
     @homeassistant_info = {url: Rails.application.secrets.homeassistant[:url], port: Rails.application.secrets.homeassistant[:port], password: Rails.application.secrets.homeassistant[:password]}
+  end
 end
