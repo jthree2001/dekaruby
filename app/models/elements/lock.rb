3660f5f9d1f65e323bb9405808bfa74d8de2cffc
jthree2001 <lawsmichael@gmail.com>
2017-10-24 22:23:48 -0400

    adding lock/unlock features

--- app/models/elements/lock.rb
+++ app/models/elements/lock.rb
@@ -6,4 +6,18 @@ class Lock < Element
   def set_type
     self.type = "Lock"
   end
+
+  def unlock
+    get_homeassistant_creds
+    body_builder ='{"entity_id": "lock.front_door"}'
+    url = "#{@homeassistant_info[:url]}:#{@homeassistant_info[:port]}/api/services/lock/unlock"
+    api_call_post url, body_builder
+  end
+
+  def lock
+    get_homeassistant_creds
+    body_builder ='{"entity_id": "'+ self.entity_id+'"}'
+    url = "#{@homeassistant_info[:url]}:#{@homeassistant_info[:port]}/api/services/lock/lock"
+    api_call_post url, body_builder
+  end
 end
