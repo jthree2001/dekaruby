0d1b01320517c5a379cc1f9da0518afba5cc07b8
jthree2001 <lawsmichael@gmail.com>
2017-11-16 21:23:50 -0500

    addnig Brain changes

--- app/models/elements/climate.rb
+++ app/models/elements/climate.rb
@@ -6,4 +6,11 @@ class Climate < Element
   def set_type
     self.type = "Climate"
   end
+
+ def set_temperature(temperature)
+   get_homeassistant_creds
+   body_builder ='{"entity_id": "'+ "#{self.entity_id}" +'",  "temperature": "'+ "#{temperature}" +'" }'
+   url = "#{@homeassistant_info[:url]}:#{@homeassistant_info[:port]}/api/services/climate/set_temperature"
+   api_call_post url, body_builder
+ end
 end
--- app/models/elements/light.rb
+++ app/models/elements/light.rb
@@ -3,4 +3,17 @@ class Light < Element
     Element.model_name
   end
 
+  def turn_on(color_name:"white", brightness_pct: "100")
+    get_homeassistant_creds
+    body_builder ='{"entity_id": "'+ self.entity_id+'", "color_name": "'+color_name+'", "brightness_pct": "'+brightness_pct+'"}'
+    url = "#{@homeassistant_info[:url]}:#{@homeassistant_info[:port]}/api/services/light/turn_on"
+    api_call_post url, body_builder
+  end
+
+  def turn_off
+    get_homeassistant_creds
+    body_builder ='{"entity_id": "'+ self.entity_id+'"}'
+    url = "#{@homeassistant_info[:url]}:#{@homeassistant_info[:port]}/api/services/light/turn_off"
+    api_call_post url, body_builder
+  end
 end
